187ba8f43da05079f5e3291eb52d151aa6c3b0a9
Matt Ruston <matt@rustonmail.com>
2017-05-13 15:57:14 -0500

    Added files for the other screens

---
+++ CreativeProject/ActivitySelectViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ActivitySelectViewController.swift
+//  CreativeProject
+//
+//  Created by Matthew Ruston on 5/13/17.
+//  Copyright © 2017 MillaVilkki. All rights reserved.
+//
+
+import UIKit
+
+class ActivitySelectViewController: UIViewController {
+    
+}
---
+++ CreativeProject/EventSelectViewController.swift
@@ -0,0 +1,9 @@
+//
+//  EventSelectViewController.swift
+//  CreativeProject
+//
+//  Created by Matthew Ruston on 5/13/17.
+//  Copyright © 2017 MillaVilkki. All rights reserved.
+//
+
+import Foundation
