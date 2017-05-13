dc24ad05424b7f9592ded996fd6d173125c3543b
Matt Ruston <matt@rustonmail.com>
2017-05-13 17:27:31 -0500

    Finished base of stage select

--- CreativeProject/StageSelectViewController.swift
+++ CreativeProject/StageSelectViewController.swift
@@ -12,6 +12,13 @@ class StageSelectViewController: UIViewController {
     
     @IBOutlet private weak var backgroundImageView: UIImageView!
     @IBOutlet private weak var characterImageView: UIImageView!
+    @IBOutlet private weak var brawnLabel: UILabel!
+    @IBOutlet private weak var enduranceLabel: UILabel!
+    @IBOutlet private weak var agilityLabel: UILabel!
+    @IBOutlet private weak var finesseLabel: UILabel!
+    @IBOutlet private weak var witsLabel: UILabel!
+    @IBOutlet private weak var magicLabel: UILabel!
+    @IBOutlet private weak var charmLabel: UILabel!
     
     let locations: [Location] = [.bar, .library, .gym, .park, .cafe, .beach, .concert, .cinema, .stadium]
     let currentCharacter: Character
@@ -33,6 +40,7 @@ class StageSelectViewController: UIViewController {
         
         backgroundImageView.image = locations[selectedLocation].image()
         characterImageView.image = currentCharacter.image
+        displayCharacterStats()
     }
     
     @IBAction private func previousLocation() {
@@ -49,6 +57,16 @@ class StageSelectViewController: UIViewController {
         UIView.transition(with: backgroundImageView, duration: 0.2, options: .transitionCrossDissolve, animations: {
             self.backgroundImageView.image = self.locations[self.selectedLocation].image()
         }, completion: nil)
+    }
+    
+    private func displayCharacterStats() {
+        brawnLabel.text = String(currentCharacter.brawn)
+        enduranceLabel.text = String(currentCharacter.endurance)
+        agilityLabel.text = String(currentCharacter.agility)
+        finesseLabel.text = String(currentCharacter.finesse)
+        witsLabel.text = String(currentCharacter.wits)
+        magicLabel.text = String(currentCharacter.magic)
+        charmLabel.text = String(currentCharacter.charm)
     }
     
 }
