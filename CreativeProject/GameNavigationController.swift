5f0ea5c2310393871a5953b6393fdd6380bd2042
Matt Ruston <matt@rustonmail.com>
2017-05-14 02:12:33 -0500

    MVP done. Just needs some testing and double check strings.

--- CreativeProject/GameNavigationController.swift
+++ CreativeProject/GameNavigationController.swift
@@ -14,9 +14,9 @@ protocol GameNavigationControllerDelegate: class {
 
 class GameNavigationController: UINavigationController {
     
-    let introText = "Hi"//"Rumors speak of a spontaneous worldwide competition, one that occurs with little warning. The last supposed occurrence featured events no nation had been able to predict such as cookie baking and horse grooming, but that was centuries ago.\nJust today, the seers appeared once again, unseen for almost three hundred years. The Miramortem Games were announced with the date set for two months down the line. They listed off the participants, and you were included as one of many to represent your nation. No hints are given as to what the nations will be tested on come time of the event, but everyone knows that placing last will have negative consequences.\n\nAfter all, the last nation to supposedly have lost no longer exists."
+    let introText = "Rumors speak of a spontaneous worldwide competition, one that occurs with little warning. The last supposed occurrence featured events no nation had been able to predict such as cookie baking and horse grooming, but that was centuries ago.\nJust today, the seers appeared once again, unseen for almost three hundred years. The Miramortem Games were announced with the date set for two months down the line. They listed off the participants, and you were included as one of many to represent your nation. No hints are given as to what the nations will be tested on come time of the event, but everyone knows that placing last will have negative consequences.\n\nAfter all, the last nation to supposedly have lost no longer exists."
     
-    let transitionText = "hi"
+    let transitionText = "The eight weeks allotted for training finally come to an end. You, along with all the other competitors, are flown to the event and brought into the stadium. The seers appear before the gathered people and announce the groups through which you will select your event along with the various activities in which you will compete. \nAfterwards, you and the rest of your group are guided to a small room where you decide who gets to do what..."
     
     fileprivate var characters: [Character] = []
     fileprivate var possibleLocations: [Location] = []
@@ -126,13 +126,13 @@ class GameNavigationController: UINavigationController {
         
         switch victoryType {
         case .overwhelmingSuccess:
-            return "This is an overwhelming success"
+            return "You can feel the confidence of your fellow countrymen as you stand on the field waiting for the results. You, personally, did very well, and, from what you heard, so did everyone else chosen from your homeland. The entire audience, and likely the world, goes silent as the Seers appear on the field holding the fate of the world in their hands. They announce each event’s outcome individually before divulging the final standing. The moment your country is revealed as the winner, cheers erupt throughout the stadium, drowning out the rest of the results. Your country celebrates the victory for months, seeing an increase in tourism as well as trainees for future competitions. Of course, the foreboding fact that your country will be forced to host the next event dampens spirits slightly once the initial joy passes."
         case .moderateSuccess:
-            return "This is a moderate success"
+            return "You stay close to your fellow competitors, mingling with others from various nations. You make small talk for a while, discussing various successes and failures in events. One of your comrades is cut off mid-sentence by the appearance of the Seers. All eyes are glued as they make their way to the podium. Result after result is announced until it is time for the final standing. While your country didn’t manage first, they managed a solid second place. The entire populace is incredibly relieved at the result, no death and no obligation to host the next occurrence of the Miramortem Games. Business sees a small boom as other nation’s encourage visitation and study in the land that avoided all punishment."
         case .success:
-            return "This is a success"
+            return "You worry about what the final standing of your country will be as the results of each individual event was a grab bag of successes and failures. The appearance of the Seers on the stadium grounds is followed by a wave of silence throughout the audience and competitors. They read the results of every single event, results no country could have predicted. The final standings are revealed shortly after. In the end, your country managed 3rd place, which, while better than last, is not what anyone hoped for. The people begins to blame the loss on the government, saying the lack of necessary facilities led to less than stellar result. You hear rumors of underground cults beginning to form, and you fear a revolution is to come."
         case .failure:
-            return "This is a failure"
+            return "You stand next to the rest of the competitors, eagerly awaiting the results with bated breath. Silence spreads throughout the stadium as the Seers appear, envelope in hand. As they announce the outcome of the each event, you slowly realize your country did not fare very well. As it comes to a close and final standings are announced, with your homeland in deadlast, you along with all of your countrymen cease to exist."
         }
     }
 }
@@ -221,7 +221,7 @@ extension GameNavigationController: ActivitySelectViewControllerDelegate {
             roundsComplete += 1
             resetLocations()
             
-            if roundsComplete >= 1 {
+            if roundsComplete >= 8 {
                 // Move on to events
                 let transitionViewController = StoryViewController(text: transitionText)
                 transitionViewController.delegate = self
